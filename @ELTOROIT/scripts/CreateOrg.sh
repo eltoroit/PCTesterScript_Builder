5057fc98127732191edf3440451623775c303369
Andres Perez <andresperezf@yahoo.com>
2019-11-05 17:53:57 -0500

    Seems to be working

--- @ELTOROIT/scripts/CreateOrg.sh
+++ @ELTOROIT/scripts/CreateOrg.sh
@@ -1,6 +1,6 @@
 # Execute in Mac using: ./EXFiles/scripts/CreateOrg.sh
 echo "*** Creating scratch Org..."
-sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --setalias soVMCheck02
+sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --setalias soVMCheck02 --durationdays=30
 echo "*** Opening scratch Org..."
 sfdx force:org:open
 echo "*** Installing ApexBridge..."
