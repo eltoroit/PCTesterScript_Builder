67f50af4a01427e06ac67929dd0777ac5a498269
Andres Perez <aperez@salesforce.com>
2022-08-09 05:58:47 -0400

    Files reorganized

---
+++ @ELTOROIT/scripts/shell/CreateOrg.sh
@@ -0,0 +1,14 @@
+# Execute in Mac using: ./EXFiles/scripts/CreateOrg.sh
+echo "*** Creating scratch Org..."
+sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --setalias soVMCheck02 --durationdays=1
+echo "*** Opening scratch Org..."
+sfdx force:org:open
+echo "*** Installing ApexBridge..."
+sfdx force:package:install --package 04t6A000002D2ElQAK -w 10
+echo "*** Pushing metadata to scratch Org..."
+sfdx force:source:push
+echo "*** Assigning permission set to your user..."
+sfdx force:user:permset:assign -n Script_Maker
+echo "*** Creating data using ETCopyData plugin"
+# sfdx ETCopyData:export -c './@ELTOROIT/data'
+sfdx ETCopyData:import -c './@ELTOROIT/data' --loglevel trace
